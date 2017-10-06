68259857e5bc3e30bb4e5d2993e14b466e035866
Ben Diep <ben_diep@hotmail.com>
2017-10-07 01:19:22 +1100

    update

--- main.py
+++ main.py
@@ -36,36 +36,15 @@ def index():
     # ----------------------
     #   Word Cloud
     # --------------------*/
-    data = [['Southern Cross', 2000000], ['Flinders Street', 100000], ['Southbank', 15000], ['Kings St', 63202]]
+    # pedestrian counts for each location
+    frequencies = {'Southern Cross': 2000000, 'Flinders Street': 100000, 'Southbank': 15000, 'Kings St': 63202 }
 
-    # create textfile
-    fo = open('wordcloud.txt', 'wb')
-    for i in range(len(data)):
-        location = data[i][0]
-        count = data[i][1]
-        scale_count = int(count/500)
-
-        for j in range(scale_count):
-            fo.write(location + " ")
-    fo.close()
-
-    d = path.dirname(__file__)
-
-    # Read the whole text.
-    text = open(path.join(d, 'wordcloud.txt')).read()
-
-    # Generate a word cloud image
-    wordcloud = WordCloud().generate(text)
-
-    # Display the generated image:
-
-    # lower max_font_size
+    # create word cloud
     wordcloud = WordCloud(width=900,
                           height=600,
-                          max_font_size=70,
                           min_font_size=4,
                           background_color=None,
-                          mode='RGBA').generate(text)
+                          mode='RGBA').generate_from_frequencies(frequencies, 70)
 
     # save to image file
     wordcloud.to_file('static/img/wordcloud.png')
