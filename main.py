d9b23390a59ebd43c0faeef157f45e7ae5fa53e8
Ben Diep <ben_diep@hotmail.com>
2017-10-06 21:20:27 +1100

    update

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
+# ---------------------------------------------------------------------------------
+#   Imports
+# -------------------------------------------------------------------------------*/
 from flask import Flask, render_template
 from flask_adminlte import AdminLTE
 import dateutil.parser
 import io
-from bigquery import BigQuery
+from flask_mysqldb import MySQL
+# from bigquery import BigQuery
 
 
-class User(object):
-    """
-    Example User object.  Based loosely off of Flask-Login's User model.
-    """
-    full_name = "Ben Diep"
-    avatar = "/static/img/user2-160x160.jpg"
-    created_at = dateutil.parser.parse("November 12, 2012")
-    
+# ---------------------------------------------------------------------------------
+#   Configuration
+# -------------------------------------------------------------------------------*/
 
+# Application
 app = Flask(__name__)
 AdminLTE(app)
 
-# This is a placeholder user object.  In the real-world, this would
-# probably get populated via ... something.
-current_user = User()
+# mySQL
+mysql = MySQL(app)
 
 
+# ---------------------------------------------------------------------------------
+#   Routes
+# -------------------------------------------------------------------------------*/
 @app.route('/')
 def index():
     print " --- START ---"
@@ -29,22 +31,12 @@ def index():
     #print 'most pop'
     #print query
 
-    return render_template('index.html', current_user=current_user)
+    cursor = mysql.connection.cursor()
 
-
-@app.route('/login')
-def login():
-    return render_template('login.html', current_user=current_user)
-
-
-@app.route('/lockscreen')
-def lockscreen():
-    return render_template('lockscreen.html', current_user=current_user)
-
-
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
 
 
