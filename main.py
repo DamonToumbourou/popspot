17a7104f5cd31efc3ca72a52e38f0de42c8bf7fb
DamonToumbourou <dtoumbourou@gmail.com>
2017-10-08 00:30:17 +1100

    lib stuff

--- main.py
+++ main.py
@@ -3,16 +3,14 @@
 # -------------------------------------------------------------------------------*/
 from flask import Flask, render_template
 from flask_adminlte import AdminLTE
-from bigquery import BigQuery
 import dateutil.parser
 import io
 import map_data
 from os import path
-from wordcloud import WordCloud
-from PIL import Image
+#from wordcloud import WordCloud
+#from PIL import Image
 from bigquery import BigQuery
-
-
+import pdb;
 # ---------------------------------------------------------------------------------
 #   Configuration
 # -------------------------------------------------------------------------------*/
@@ -40,6 +38,7 @@ def white_color_func(word, font_size, position, orientation, random_state=None, 
 @app.route('/')
 def index():
 
+
     # ----------------------
     #   BigQuery
     # --------------------*/
@@ -49,6 +48,7 @@ def index():
     most_pop_month = query.most_popular_month()
     most_pop_time = query.most_popular_time()
 
+    """
     # ----------------------
     #   Word Cloud
     # --------------------*/
@@ -66,13 +66,13 @@ def index():
 
     # save to image file
     wordcloud.to_file('static/img/wordcloud.png')
-
+    """
     # -----------------------------------------------------------
     #   Fetch Data for Google Map using Melbourne City Data API
     # ---------------------------------------------------------*/
     avg_daily_traffic = map_data.MapData().get_average_daily_traffic()
     #avg = query.get_average_daily_traffic()
-
+    #avg_daily_traffic = ""
 
     return render_template('index.html', most_pop_overall=most_pop_overall, most_pop_day=most_pop_day,
                            most_pop_month=most_pop_month, most_pop_time=most_pop_time, avg_daily_traffic=avg_daily_traffic)
