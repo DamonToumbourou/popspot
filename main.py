87376688f5c74cecbcf6164e99b3f7b10c594242
Ben Diep <ben_diep@hotmail.com>
2017-10-08 23:37:52 +1100

    latest

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 from flask import Flask, render_template
 from flask_adminlte import AdminLTE
 import map_data
-#from wordcloud import WordCloud
-#from PIL import Image
+# from wordcloud import WordCloud
+# from PIL import Image
 from bigquery import BigQuery
 
 # ---------------------------------------------------------------------------------
@@ -15,9 +15,6 @@ from bigquery import BigQuery
 # Application
 app = Flask(__name__)
 AdminLTE(app)
-
-
-
 
 
 # ---------------------------------------------------------------------------------
@@ -34,7 +31,6 @@ def white_color_func(word, font_size, position, orientation, random_state=None, 
 # -------------------------------------------------------------------------------*/
 @app.route('/')
 def index():
-
     # ----------------------
     #   BigQuery
     # --------------------*/
@@ -44,12 +40,21 @@ def index():
     most_pop_month = query.most_popular_month()
     most_pop_time = query.most_popular_time()
 
-    """
+    # -----------------------------------------------------------
+    #   Fetch Data for Google Map using Melbourne City Data API
+    # ---------------------------------------------------------*/
+    avg_daily_traffic = map_data.MapData().get_average_daily_traffic()
+    # avg = query.get_average_daily_traffic()
+
+
     # ----------------------
     #   Word Cloud
     # --------------------*/
+
     # pedestrian counts for each location
-    frequencies = {'SOUTHERN CROSS': 2000000, 'FLINDERS STREET': 100000, 'SOUTH BANK': 15000, 'KINGS ST': 63202 }
+    frequencies = {}
+    for location in avg_daily_traffic:
+        frequencies[location[0]] = int(location[1])
 
     # create word cloud
     wordcloud = WordCloud(width=1856,
@@ -62,13 +67,6 @@ def index():
 
     # save to image file
     wordcloud.to_file('static/img/wordcloud.png')
-    """
-
-    # -----------------------------------------------------------
-    #   Fetch Data for Google Map using Melbourne City Data API
-    # ---------------------------------------------------------*/
-    avg_daily_traffic = map_data.MapData().get_average_daily_traffic()
-
 
     return render_template('index.html', most_pop_overall=most_pop_overall, most_pop_day=most_pop_day,
                            most_pop_month=most_pop_month, most_pop_time=most_pop_time, avg_daily_traffic=avg_daily_traffic)
