ec5e0bf04f2f38ddc02ce160c87f07eb774fab23
DamonToumbourou <dtoumbourou@gmail.com>
2017-10-08 22:42:54 +1100

    test data

--- bigquery.py
+++ bigquery.py
@@ -8,36 +8,37 @@ import uuid
 class BigQuery(object):
 
     def most_popular_overall(self):
-        client = bigquery.Client()
-        query_job = client.run_async_query(str(uuid.uuid4()), """
-        #standardSQL
-        SELECT sensor_name, SUM(hourly_counts) AS sum
-        FROM `s3462344-a1-lt11.ped_data.ped_data_small`
-        WHERE date_time LIKE '%2017%'
-        GROUP BY (sensor_name)
-        ORDER BY (sum) DESC
-        LIMIT 1;
-        """)
+        """
+        Live data commented out to save money on account
+        """
+        ##standardSQL
+        #SELECT sensor_name, SUM(hourly_counts) AS sum
+        #FROM `s3462344-a1-lt11.ped_data.ped_data_small`
+        #WHERE date_time LIKE '%2017%'
+        #GROUP BY (sensor_name)
+        #ORDER BY (sum) DESC
+        #LIMIT 1;
+        #""")
 
-        query_job.begin()
-        query_job.result()  # Wait for job to complete.
+        #query_job.begin()
+        #query_job.result()  # Wait for job to complete.
 
-        destination_table = query_job.destination
-        destination_table.reload()
+        #destination_table = query_job.destination
+        #destination_table.reload()
 
-        most_pop = []
-        for row in destination_table.fetch_data():
-            pop = {
-                'sensor_name': row[0],
-                'count': row[1]
-            }
-            most_pop.append(pop)
+        #most_pop = []
+        #for row in destination_table.fetch_data():
+        #    pop = {
+        #        'sensor_name': row[0],
+        #        'count': row[1]
+        #    }
+        #    most_pop.append(pop)
+        #
 
-        print most_pop
         most_pop_overall = []
         pop = {
-                'sensor_name': most_pop[0]['sensor_name'],
-                'count': most_pop[0]['count']
+                'sensor_name': 'Southbank',
+                'count': 11003517
             }
         most_pop_overall.append(pop)
 
@@ -45,36 +46,39 @@ class BigQuery(object):
 
 
     def most_popular_day(self):
-        client = bigquery.Client()
-        query_job = client.run_async_query(str(uuid.uuid4()), """
-        #standardSQL
-        SELECT day, sensor_name, AVG(hourly_counts) AS sum  
-        FROM `s3462344-a1-lt11.ped_data.ped_data_small`
-        GROUP BY day, sensor_name
-        ORDER BY(sum) DESC
-        LIMIT 1;
-        """)
+        """
+        Live data commented out to save money on account
+        """
+        #client = bigquery.Client()
+        #query_job = client.run_async_query(str(uuid.uuid4()), """
+        ##standardSQL
+        #SELECT day, sensor_name, AVG(hourly_counts) AS sum
+        #FROM `s3462344-a1-lt11.ped_data.ped_data_small`
+        #GROUP BY day, sensor_name
+        #ORDER BY(sum) DESC
+        #LIMIT 1;
+        #""")
+        #
+        #query_job.begin()
+        #query_job.result()  # Wait for job to complete.
+        #
+        #destination_table = query_job.destination
+        #destination_table.reload()
 
-        query_job.begin()
-        query_job.result()  # Wait for job to complete.
+        #most_pop = []
+        #for row in destination_table.fetch_data():
+        #    pop = {
+        #        'day': row[0],
+        #        'sensor_name': row[1],
+        #        'count': row[2]
+        #    }
+        #    most_pop.append(pop)
 
-        destination_table = query_job.destination
-        destination_table.reload()
-
-        most_pop = []
-        for row in destination_table.fetch_data():
-            pop = {
-                'day': row[0],
-                'sensor_name': row[1],
-                'count': row[2]
-            }
-            most_pop.append(pop)
-        print most_pop
         most_pop_day = []
         pop = {
-                'sensor_name': most_pop[0]['sensor_name'],
-                'count': int(most_pop[0]['count']),
-                'day': most_pop[0]['day']
+                'sensor_name': 'Flinders St Swanston St',
+                'count': '2547',
+                'day': 'Sunday'
             }
         most_pop_day.append(pop)
 
@@ -82,79 +86,85 @@ class BigQuery(object):
 
 
     def most_popular_time(self):
-        client = bigquery.Client()
-        query_job = client.run_async_query(str(uuid.uuid4()), """
-        #standardSQL
-        SELECT time, sensor_name, sum(hourly_counts) AS sum  
-        FROM `s3462344-a1-lt11.ped_data.ped_data_small`
-        GROUP BY time, sensor_name
-        ORDER BY(sum) DESC
-        LIMIT 1;
-        """)
+        """
+        Live data commented out to save money on account
+        """
+        #client = bigquery.Client()
+        #query_job = client.run_async_query(str(uuid.uuid4()), """
+        ##standardSQL
+        #SELECT time, sensor_name, sum(hourly_counts) AS sum
+        #FROM `s3462344-a1-lt11.ped_data.ped_data_small`
+        #GROUP BY time, sensor_name
+        #ORDER BY(sum) DESC
+        #LIMIT 1;
+        #""")
 
-        query_job.begin()
-        query_job.result()  # Wait for job to complete.
+        #query_job.begin()
+        #query_job.result()  # Wait for job to complete.
 
-        destination_table = query_job.destination
-        destination_table.reload()
+        #destination_table = query_job.destination
+        #destination_table.reload()
 
-        most_pop = []
-        for row in destination_table.fetch_data():
-            pop = {
-                'time': row[0],
-                'sensor_name': row[1],
-                'count': row[2]
-            }
-            most_pop.append(pop)
+        #most_pop = []
+        #for row in destination_table.fetch_data():
+        #    pop = {
+        #        'time': row[0],
+        #        'sensor_name': row[1],
+        #        'count': row[2]
+        #    }
+        #    most_pop.append(pop)
 
+
+        #time = most_pop[0]['time']
+        #if time > 12:
+        #    time = str(time - 12) + ":00 PM"
+        #if time <= 12:
+        #    time = str(time) + ":00 AM"
         most_pop_time = []
-        time = most_pop[0]['time']
-        if time > 12:
-            time = str(time - 12) + ":00 PM"
-        if time <= 12:
-            time = str(time) + ":00 AM"
-        print type(time)
         pop = {
-                'sensor_name': most_pop[0]['sensor_name'],
-                'count': int(most_pop[0]['count']),
-                'time': time
-            }
+                'sensor_name': 'Southbank',
+                'count': '1059289',
+                'time': '5:00 PM'
+        }
         most_pop_time.append(pop)
 
         return most_pop_time
 
 
     def most_popular_month(self):
-        client = bigquery.Client()
-        query_job = client.run_async_query(str(uuid.uuid4()), """
-        #standardSQL
-        SELECT month, sensor_name, sum(hourly_counts) AS sum  
-        FROM `s3462344-a1-lt11.ped_data.ped_data_small`
-        GROUP BY month, sensor_name
-        ORDER BY(sum) DESC
-        LIMIT 1;
-        """)
+        """
+        Live data commented out to save money on account
+        """
+        #client = bigquery.Client()
+        #query_job = client.run_async_query(str(uuid.uuid4()), """
+        ##standardSQL
+        #SELECT month, sensor_name, sum(hourly_counts) AS sum
+        #FROM `s3462344-a1-lt11.ped_data.ped_data_small`
+        #GROUP BY month, sensor_name
+        #ORDER BY(sum) DESC
+        #LIMIT 1;
+        #""")
 
-        query_job.begin()
-        query_job.result()  # Wait for job to complete.
+        #query_job.begin()
+        #query_job.result()  # Wait for job to complete.
 
-        destination_table = query_job.destination
-        destination_table.reload()
+        #destination_table = query_job.destination
+        #destination_table.reload()
 
-        most_pop = []
-        for row in destination_table.fetch_data():
-            pop = {
-                'month': row[0],
-                'sensor_name': row[1],
-                'count': row[2]
-            }
-            most_pop.append(pop)
+        #most_pop = []
+        #for row in destination_table.fetch_data():
+        #    pop = {
+        #        'month': row[0],
+        #        'sensor_name': row[1],
+        #        'count': row[2]
+        #    }
+        #    most_pop.append(pop)
 
         most_pop_month = []
         pop = {
-                'sensor_name': most_pop[0]['sensor_name'],
-                'count': most_pop[0]['count'],
-                'month': most_pop[0]['month']
+                'sensor_name': 'Flinders La Swanston St',
+                'count': '1504540',
+                'month': 'July'
             }
         most_pop_month.append(pop)
 
@@ -162,31 +172,35 @@ class BigQuery(object):
 
 
     def get_average_daily(self):
-        client = bigquery.Client()
-        query_job = client.run_async_query(str(uuid.uuid4()), """
-        #standardSQL
-        SELECT sensor_name, AVG(hourly_counts) AS sum
-        FROM `s3462344-a1-lt11.ped_data.ped_data_small`
-        GROUP BY sensor_name
-        ORDER BY(sum) DESC;
-        """)
+        """
+        Live data commented out to save money on account
+        """
+        #client = bigquery.Client()
+        #query_job = client.run_async_query(str(uuid.uuid4()), """
+        ##standardSQL
+        #SELECT sensor_name, AVG(hourly_counts) AS sum
+        #FROM `s3462344-a1-lt11.ped_data.ped_data_large`
+        #GROUP BY sensor_name
+        #ORDER BY(sum) DESC;
+        #""")
 
-        query_job.begin()
-        query_job.result()  # Wait for job to complete.
+        #query_job.begin()
+        #query_job.result()  # Wait for job to complete.
 
-        destination_table = query_job.destination
-        destination_table.reload()
+        #destination_table = query_job.destination
+        #destination_table.reload()
+        #print destination_table.fetch_data()
 
-        average_daily = []
-        for row in destination_table.fetch_data():
-            pop = {
-                'sensor_name': row[0],
-                'count': row[1]
+        #average_daily = []
+        #for row in destination_table.fetch_data():
+        #    pop = {
+        #        'sensor_name': row[0],
+        #        'count': row[1]
 
-            }
-            average_daily.append(pop)
+        #    }
+        #    average_daily.append(pop)
 
-        # test data
-        #average_daily = [{'count': 2188.7500000000005, 'sensor_name': u'Flinders St-Swanston St (West)'}, {'count': 1954.769474637681, 'sensor_name': u'Flinders La-Swanston St (West)'}, {'count': 1679.4134615384626, 'sensor_name': u'Southbank'}, {'count': 1506.0472222222234, 'sensor_name': u'Flinders St-Elizabeth St (East)'}, {'count': 1444.3820301783203, 'sensor_name': u'Flinders Street Station Underpass'}, {'count': 1402.7851979345967, 'sensor_name': u'Town Hall (West)'}, {'count': 1352.0171081677686, 'sensor_name': u'Princes Bridge'}, {'count': 1319.4600480109718, 'sensor_name': u'Bourke Street Mall (North)'}, {'count': 1312.4513031550105, 'sensor_name': u'Spencer St-Collins St (North)'}, {'count': 1306.3314926372152, 'sensor_name': u'Melbourne Central'}, {'count': 1064.7357843137243, 'sensor_name': u'The Arts Centre'}, {'count': 1049.8731884057975, 'sensor_name': u'City Square'}, {'count': 994.2004458161869, 'sensor_name': u'Bourke Street Mall (South)'}, {'count': 880.9058641975291, 'sensor_name': u'State Library'}, {'count': 880.6561302681989, 'sensor_name': u'Little Collins St-Swanston St (East)'}, {'count': 827.1465517241365, 'sensor_name': u'Flagstaff Station'}, {'count': 713.145121082622, 'sensor_name': u'Australia on Collins'}, {'count': 656.3830589849114, 'sensor_name': u'Southern Cross Station'}, {'count': 644.1575788751732, 'sensor_name': u'Bourke St-Russell St (West)'}, {'count': 606.1469478738024, 'sensor_name': u'Chinatown-Swanston St (North)'}, {'count': 541.4305555555574, 'sensor_name': u'QV Market-Elizabeth St (West)'}, {'count': 530.1975308641983, 'sensor_name': u'Melbourne Convention Exhibition Centre'}, {'count': 516.6469726562509, 'sensor_name': u'Lonsdale St (South)'}, {'count': 512.3187037037042, 'sensor_name': u'Birrarung Marr'}, {'count': 474.8844307270246, 'sensor_name': u'Collins Place (South)'}, {'count': 447.2153041825081, 'sensor_name': u'St Kilda Rd-Alexandra Gardens'}, {'count': 430.12236842105307, 'sensor_name': u'Sandridge Bridge'}, {'count': 365.56436011904674, 'sensor_name': u'Spencer St-Collins St (South)'}, {'count': 363.8033264746235, 'sensor_name': u'Chinatown-Lt Bourke St (South)'}, {'count': 354.1961591220842, 'sensor_name': u'Collins Place (North)'}, {'count': 291.6956018518514, 'sensor_name': u'Lonsdale St-Spring St (West)'}, {'count': 286.51115023474256, 'sensor_name': u'Queen St (West)'}, {'count': 275.4308785529714, 'sensor_name': u'Lygon St (West)'}, {'count': 251.76617826617877, 'sensor_name': u'Grattan St-Swanston St (West)'}, {'count': 236.29991356957646, 'sensor_name': u'Alfred Place'}, {'count': 225.38629426129424, 'sensor_name': u'Monash Rd-Swanston St (West)'}, {'count': 209.07355967078223, 'sensor_name': u'New Quay'}, {'count': 183.35339506172832, 'sensor_name': u'Victoria Point'}, {'count': 173.97405372405353, 'sensor_name': u'Flinders St-Spring St (West)'}, {'count': 167.82387057387015, 'sensor_name': u'Lygon St (East)'}, {'count': 163.2606209150332, 'sensor_name': u'Webb Bridge'}, {'count': 149.967032967033, 'sensor_name': u'Flinders St-Spark La'}, {'count': 125.99053724053675, 'sensor_name': u'QV Market-Peel St'}, {'count': 122.91358024691361, 'sensor_name': u'Pelham St (S)'}, {'count': 116.64239926739889, 'sensor_name': u'Tin Alley-Swanston St (West)'}, {'count': 67.5252677376171, 'sensor_name': u'Waterfront City'}]
+        #test data
+        average_daily = [{'count': 2188.7500000000005, 'sensor_name': u'Flinders St-Swanston St (West)'}, {'count': 1954.769474637681, 'sensor_name': u'Flinders La-Swanston St (West)'}, {'count': 1679.4134615384626, 'sensor_name': u'Southbank'}, {'count': 1506.0472222222234, 'sensor_name': u'Flinders St-Elizabeth St (East)'}, {'count': 1444.3820301783203, 'sensor_name': u'Flinders Street Station Underpass'}, {'count': 1402.7851979345967, 'sensor_name': u'Town Hall (West)'}, {'count': 1352.0171081677686, 'sensor_name': u'Princes Bridge'}, {'count': 1319.4600480109718, 'sensor_name': u'Bourke Street Mall (North)'}, {'count': 1312.4513031550105, 'sensor_name': u'Spencer St-Collins St (North)'}, {'count': 1306.3314926372152, 'sensor_name': u'Melbourne Central'}, {'count': 1064.7357843137243, 'sensor_name': u'The Arts Centre'}, {'count': 1049.8731884057975, 'sensor_name': u'City Square'}, {'count': 994.2004458161869, 'sensor_name': u'Bourke Street Mall (South)'}, {'count': 880.9058641975291, 'sensor_name': u'State Library'}, {'count': 880.6561302681989, 'sensor_name': u'Little Collins St-Swanston St (East)'}, {'count': 827.1465517241365, 'sensor_name': u'Flagstaff Station'}, {'count': 713.145121082622, 'sensor_name': u'Australia on Collins'}, {'count': 656.3830589849114, 'sensor_name': u'Southern Cross Station'}, {'count': 644.1575788751732, 'sensor_name': u'Bourke St-Russell St (West)'}, {'count': 606.1469478738024, 'sensor_name': u'Chinatown-Swanston St (North)'}, {'count': 541.4305555555574, 'sensor_name': u'QV Market-Elizabeth St (West)'}, {'count': 530.1975308641983, 'sensor_name': u'Melbourne Convention Exhibition Centre'}, {'count': 516.6469726562509, 'sensor_name': u'Lonsdale St (South)'}, {'count': 512.3187037037042, 'sensor_name': u'Birrarung Marr'}, {'count': 474.8844307270246, 'sensor_name': u'Collins Place (South)'}, {'count': 447.2153041825081, 'sensor_name': u'St Kilda Rd-Alexandra Gardens'}, {'count': 430.12236842105307, 'sensor_name': u'Sandridge Bridge'}, {'count': 365.56436011904674, 'sensor_name': u'Spencer St-Collins St (South)'}, {'count': 363.8033264746235, 'sensor_name': u'Chinatown-Lt Bourke St (South)'}, {'count': 354.1961591220842, 'sensor_name': u'Collins Place (North)'}, {'count': 291.6956018518514, 'sensor_name': u'Lonsdale St-Spring St (West)'}, {'count': 286.51115023474256, 'sensor_name': u'Queen St (West)'}, {'count': 275.4308785529714, 'sensor_name': u'Lygon St (West)'}, {'count': 251.76617826617877, 'sensor_name': u'Grattan St-Swanston St (West)'}, {'count': 236.29991356957646, 'sensor_name': u'Alfred Place'}, {'count': 225.38629426129424, 'sensor_name': u'Monash Rd-Swanston St (West)'}, {'count': 209.07355967078223, 'sensor_name': u'New Quay'}, {'count': 183.35339506172832, 'sensor_name': u'Victoria Point'}, {'count': 173.97405372405353, 'sensor_name': u'Flinders St-Spring St (West)'}, {'count': 167.82387057387015, 'sensor_name': u'Lygon St (East)'}, {'count': 163.2606209150332, 'sensor_name': u'Webb Bridge'}, {'count': 149.967032967033, 'sensor_name': u'Flinders St-Spark La'}, {'count': 125.99053724053675, 'sensor_name': u'QV Market-Peel St'}, {'count': 122.91358024691361, 'sensor_name': u'Pelham St (S)'}, {'count': 116.64239926739889, 'sensor_name': u'Tin Alley-Swanston St (West)'}, {'count': 67.5252677376171, 'sensor_name': u'Waterfront City'}]
 
         return average_daily
